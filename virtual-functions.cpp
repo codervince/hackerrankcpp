9f868c2f0ab587b6b7500a5781da6784a68c1d14
codervince <publicvince102@gmail.com>
2016-01-29 06:19:03 +0100

    Update virtual-functions.cpp

--- virtual-functions.cpp
+++ virtual-functions.cpp
@@ -1,38 +1,68 @@
 class Person {
   public:
-       virtual void getdata() const {}
-       virtual void putdata() const {}
+        Person()
+        {
+        }
+       virtual void getdata() const {cout << "Person getdata()";}
+       virtual void putdata() const {cout << "Person putdata()";}
+       virtual void setage(int theage){
+           age = theage;
+       }
+       virtual int getage() {
+           return age;
+       }
+       virtual void setname(string thename){
+           name = thename;
+       }
+       virtual string getname() {
+           return name;
+       } 
   protected:
     string name;
     int age;
 };
 
+class Student: public Person
+{
+public:
+    Student() {curid++;}
+
+protected:
+    int marks[6];
+    static int curid;
+};
+
 class Professor: public Person
 {
- public:
-    Professor(){
-        cur_{id}++;
-    }
+    public:
+        Professor(){}
+    
+        void getdata() const {
+            string inname;
+            int inage;
+            int inpublications;
+            //professor has name age publications
+            cin >> inname;
+            cin >> inage;
+            cin >> inpublications;
+            cout << inname << "here" << inage<< endl;
+            //name, age is updated in Person - publications
+            //create Person
 
-    void getdata() {
-        string inname;
-        int inage;
-        int inpublications;
-        //professor has name age publications
-        cin >> inname;
-        cin >> inage;
-        cin >> inpublications;
-        //name, age is updated in Person - publications
-        Person::get
-        publications = inpublications;
-        //id is updated by constructor
+            //publications = inpublications;
+            //id is updated by constructor
     }
     
-    void putdate(){
-        //print out name age publications id
-        
-    }
+        virtual void putdata(){
+            //print out name age publications id
+            cout << "called here";
+            cout << Person::getname() << " " << Person::getage() << " " << publications << " " << curid << endl;
+
+        }
  protected:
     int publications;
-    static int cur_{id};
+    static int curid;
+
 };
+int Professor::curid = 0;
+int Student::curid = 0;
