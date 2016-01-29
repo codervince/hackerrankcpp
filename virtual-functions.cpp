1e5ddcf923cbe6a66bfc91e34f822923c8c9ae18
codervince <publicvince102@gmail.com>
2016-01-29 07:24:43 +0100

    Update virtual-functions.cpp
    added test driver
    passes test case 1
    4
    1
    Walter 56 99
    2
    Jesse 18 50 48 97 76 34 98
    2
    Pinkman 22 10 12 0 18 45 50
    1
    White 58 87

--- virtual-functions.cpp
+++ virtual-functions.cpp
@@ -1,3 +1,12 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+//my code
+//task https://www.hackerrank.com/contests/code-cpp-3/challenges/virtual-functions
 class Person {
   public:
         Person()
@@ -26,8 +35,29 @@ class Student: public Person
 {
 public:
     Student() {curid++;}
+    
+    void getdata(){
+        string inname;
+        int inage;
+        int i;
+        cin >> inname;
+        cin >> inage;
+        name = inname;
+        age = inage;
+        for (i= 0; i< 6; i++){
+            int val;
+            cin >> val;
+            summarks = summarks + val;
+            marks[i] = val;
+        }
+    }
+    
+    void putdata(){
+        cout << name << " " << age << " " << summarks << " " << curid << endl;
+    }
 
 protected:
+    int summarks=0;
     int marks[6];
     static int curid;
 };
@@ -35,7 +65,7 @@ protected:
 class Professor: public Person
 {
     public:
-        Professor(){}
+        Professor(){curid++;}
     
         void getdata() {
             string inname;
@@ -45,22 +75,15 @@ class Professor: public Person
             cin >> inname;
             cin >> inage;
             cin >> inpublications;
-            cout << inname << "here " << inage<< endl;
             name = inname;
             age = inage;
-            
-            //name, age is updated in Person - publications
-            //create Person
+            publications = inpublications;
 
-            //publications = inpublications;
-            //id is updated by constructor
     }
     
         void putdata(){
-            //print out name age publications id
-            cout << "called here";
-            cout << name << " " << age << endl;
-            //cout << getname() << " " << getage() << " " << publications << " " << curid << endl;
+            cout << name << " " << age << " " << publications << " " << curid << endl;
+
 
         }
  protected:
@@ -70,3 +93,31 @@ class Professor: public Person
 };
 int Professor::curid = 0;
 int Student::curid = 0;
+
+//test driver (given)
+int main(){
+
+    int n, val;
+    cin>>n; //The number of objects that is going to be created.
+    Person *per[n];
+
+    for(int i = 0;i < n;i++){
+
+        cin>>val;
+        if(val == 1){
+            // If val is 1 current object is of type Professor
+            per[i] = new Professor;
+
+        }
+        else per[i] = new Student; // Else the current object is of type Student
+
+        per[i]->getdata(); // Get the data from the user.
+
+    }
+
+    for(int i=0;i<n;i++)
+        per[i]->putdata(); // Print the required output for each object.
+
+    return 0;
+
+}
